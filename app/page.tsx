f2b401c8701416847774c83ab00c61454b9bec1d
D Dilip Kumar <d.kumar@dsl.com>
2026-03-10 16:39:25 +0530

    Fix component imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,6 @@
-import Hero from "@/components/Hero";
-import FeatureGrid from "@/components/FeatureGrid";
-import Trust from "@/components/Trust";
-import CTA from "@/components/CTA";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-
-export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <FeatureGrid />
-      <Trust />
-      <CTA />
-      <Footer />
-    </main>
-  );
-}
+import Hero from "../components/Hero";
+import FeatureGrid from "../components/FeatureGrid";
+import Trust from "../components/Trust";
+import CTA from "../components/CTA";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
